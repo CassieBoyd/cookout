f14155c006a4a8f50bb4709ba226cbdbcb850898
Cassie Boyd <cassandra.bacon@gmail.com>
2019-10-14 13:08:38 -0500

    Got for each  method working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,27 @@
 const hamburger = {
-	name: 'Hamburger',
-	type: 'beef',
-	cooked: false,
+    name: 'Hamburger',
+    type: 'beef',
+    cooked: false,
 }
 const zucchini = {
-	name: 'Zucchini',
-	type: 'vegetable',
-	cooked: false,
+    name: 'Zucchini',
+    type: 'vegetable',
+    cooked: false,
 }
 const chickenBreast = {
-	name: 'Chicken Breast',
-	type: 'chicken',
-	cooked: false,
+    name: 'Chicken Breast',
+    type: 'chicken',
+    cooked: false,
 }
 const corn = {
-	name: 'Corn',
-	type: 'vegetable',
-	cooked: false,
+    name: 'Corn',
+    type: 'vegetable',
+    cooked: false,
 }
 const steak = {
-	name: 'Steak',
-	type: 'beef',
-	cooked: false,
+    name: 'Steak',
+    type: 'beef',
+    cooked: false,
 }
 
 // An array that is grouping the objects together.
@@ -30,10 +30,18 @@ const foods = [hamburger, zucchini, chickenBreast, corn, steak];
 // An empty array that will store the objects after the `grill()` function cooks the food.
 const cookedFood = [];
 
-function grill (currentObject) {
+function grill(currentObject) {
     // Modify the food so that it is cooked
     currentObject.cooked = true;
 
     // Put the cooked food into the appropriate array
     cookedFood.push(currentObject);
 };
+
+// For each method executes grill function on foods array causing it to be cooked
+foods.forEach(grill);
+
+// For loop loops through cookedFood array and logs it to the console
+for (i = 0; i < cookedFood.length; i++) {
+    console.log(cookedFood[i]);
+}
